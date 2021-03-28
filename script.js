e1fdbf36586926fdf7aa44962f704410d6e207a3
Agustin Di Benedetto <agustindibenedetto98@gmail.com>
2021-03-28 09:30:45 -0300

    local sotrage

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 var agregarBoton, cancelarTarea;
 
+function saveLocalStorage () {
+    let todo =  $("#lista").html();
+    console.log(todo);
+    localStorage.setItem("todo" , todo);
+}
+
+function rescueLocalStorge(){
+    $("#lista").append(localStorage.getItem ("todo"));
+}
+
 function agregar(){
     let valor = $('#tarea').val();
     console.log(valor);
     if( valor != '' ){
         $("#lista").append  (`
                             <div class="lista__tarea">
-                                <div onclick = "this.parentElement.remove()" class="lista__tarea__cancelar">
+                                <div onclick = "this.parentElement.remove();saveLocalStorage();" class="lista__tarea__cancelar">
                                     <i " class="fas fa-window-close"></i>
                                 </div>
                                 <div class="lista__tarea__texto">
@@ -18,7 +28,9 @@ function agregar(){
     else{
         alert("Ingresa tarea");
     }
+    saveLocalStorage();
 }
+
 
 $(document).ready(function (){
     $(".agregar__boton").click(agregar);
@@ -26,5 +38,8 @@ $(document).ready(function (){
     $(".borrar__todo__boton").click(()=>{
         $(".lista__tarea").remove();
     });
+
+    rescueLocalStorge();
+
     
 })
